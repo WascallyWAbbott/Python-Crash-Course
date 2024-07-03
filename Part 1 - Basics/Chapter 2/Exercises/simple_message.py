03b173dbb5655ada84e933bf98ac76427e169702
WascallyWAbbott <46385462+WascallyWAbbott@users.noreply.github.com>
2024-07-03 12:12:53 -0700

    Removed brackets line 3

---
+++ Part 1 - Basics/Chapter 2/Exercises/simple_message.py
@@ -0,0 +1,4 @@
+# Assign a variable to a message, then print variable.
+
+simple_message = {"This is a simple message"}
+print(simple_message)
--- Part 1 - Basics/Chapter 2/simple_message.py
+++
@@ -1,4 +0,0 @@
-# Assign a variable to a message, then print variable.
-
-simple_messasge = {"This is a simple message"}
-print(simple_messasge)
