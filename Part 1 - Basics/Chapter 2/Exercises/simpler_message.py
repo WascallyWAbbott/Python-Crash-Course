7fd42712e8919a5b70fdf910cacc11078f67c2a6
WascallyWAbbott <46385462+WascallyWAbbott@users.noreply.github.com>
2024-07-03 12:17:24 -0700

    first

---
+++ Part 1 - Basics/Chapter 2/Exercises/simpler_message.py
@@ -0,0 +1,9 @@
+# Assign a variable to a message, then print variable.
+
+simple_message = "This is a simple message"
+print(simple_message)
+
+# Assign a new variable to message, using = then print
+
+simple_message = "Even simpler message"
+print(simple_message)
